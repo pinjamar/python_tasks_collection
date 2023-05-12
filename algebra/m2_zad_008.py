4a3372596e4d1f0b5016aebf00266e439b0f8318
Ivan <pinjamar@gmail.com>
2023-05-12 21:14:46 +0200

    m2 algebra bootcamp

---
+++ algebra/m2_zad_008.py
@@ -0,0 +1,15 @@
+#ip adresa = 192.168.0.184
+
+prvi_oktet = bin(192)
+drugi_oktet = bin(168)
+treci_oktet = bin(0)
+cetvrti_oktet = bin(184)
+
+print(prvi_oktet, drugi_oktet, treci_oktet, cetvrti_oktet, sep=".")
+
+prvi_oktet_o = oct(192)
+drugi_oktet_o = oct(168)
+treci_oktet_o = oct(0)
+cetvrti_oktet_o = oct(184)
+
+print(prvi_oktet_o, drugi_oktet_o, treci_oktet_o, cetvrti_oktet_o, sep=".")
---
+++ algebra/m2_zad_009.py
@@ -0,0 +1,5 @@
+ime = "Filp"
+prezime = "Skenderovic"
+puno_ime = "Puno ime i prezime je: {} {}".format(ime,prezime)
+
+print(puno_ime)
