e1be4772d670a3da9c8f6c216b5ca8e753bc09fb
Ivan <pinjamar@gmail.com>
2023-06-19 21:20:31 +0200

    bankovni racun exam i zbirka dodana

--- algebra/bankovni_racun.py
+++ algebra/bankovni_racun.py
@@ -26,6 +26,7 @@ account_number = ''
 account_balance = 0.00
 
 def generate_account_number():
+    global account_number
     year = datetime.datetime.now().year
     month = datetime.datetime.now().month
     if month < 10:
@@ -74,9 +75,6 @@ def main_menu():
             choice = int(input('Molimo unesite broj između 0 i 5!'))
 
     return choice
-
-def create_transaction():
-    pass
 
 def kreiranje_racuna_menu():
     print('*' * 65)
@@ -137,20 +135,21 @@ def open_account():
     if amount != '':
         amount = float(amount)
 
-        transaction = []
+        transactions = []
         account_balance += amount
 
         # transakcija - dodajemo datum, vrijeme, iznos, stanje, broj racuna, opis, ime managera
-        transaction.append(datetime.datetime.date)
-        transaction.append(datetime.datetime.time)
-        transaction.append(amount)
-        transaction.append(account_balance)
-        transaction.append(account_number)
-        transaction.append('Polog kod otvaranja računa')
-        transaction.append(company_manager)
-        transactions[transaction_id + 1] = transaction
+        transactions.append(datetime.datetime.date)
+        transactions.append(datetime.datetime.time)
+        transactions.append(amount)
+        transactions.append(account_balance)
+        transactions.append(account_number)
+        transactions.append('Polog kod otvaranja računa')
+        transactions.append(company_manager)
+        transactions[transaction_id + 1] = transactions
     else:
         amount = 0.00
+  
   
 def display_account_balance():
     clear_terminal()
@@ -159,33 +158,133 @@ def display_account_balance():
     print('Prikaz stanja računa\n'.center(65))
 
     print(f'Broj racuna\t{account_number}')
-    print(f'Datumi vrijeme:\t{datetime.datetime.now()}\n')
+    print(f'Datum i vrijeme:\t{datetime.datetime.now()}\n')
 
     print(f'Trenutno stanje racuna:\t{account_balance}{currency}\n\n')
 
-    print('-') * 65
+    print('_' * 65)
     input('Za povratak u glavni izbornik pritisnite bilo koju tipku\t')
+    
+    
+def update_account():
+    clear_terminal()
+    print('-' * 65)
+    print('Pybank Algebra\n'.center(65),'\n')
+    print('Azuriranje racuna\n'.center(65), '\n')
+    
+    global company_name
+    global company_street_and_number
+    global company_postal_code
+    global company_city
+    global company_tax_id
+    global company_manager
+    global currency
+    
+    company_name = input('Unesite novi naziv tvrtke:\t\t')
+    company_street_and_number = input('Unesite ulicu i broj tvrtke:\t\t')
+    company_postal_code = input('Unesite postanski broj tvrtke:\t\t')
+    company_city = input('Unesite grad u kojem je tvrtka:\t\t')
+    company_tax_id = input('Unesite OIB tvrtke:\t\t')
+    while len(company_tax_id) != 11 and company_tax_id.isdigit():
+        print('OIB mora imati tocno 11 znakova i mora sadrzavati smao brojeve. Pokusajte ponovno.')
+        company_tax_id = input('Unesite OIB tvrtke:\t\t')
+    company_manager = input('Unesite puno ime osobe odgovorne za tvrtku:\t\t')
+    currency = input('Unesite naziv valute racuna - EUR ili HRK:\t\t')
+    if currency.upper() == 'HRK':
+        currency = 'hr'
+    else:
+        currency = '€'
+        
+    input('Za povratak u glavni izbornik pritisnite bilo koju tipku\t')
+    clear_terminal()
+    print(f'Podaci o firmi {company_name} su uspješno promijenjeni.')
+    input('Za nastavak pritisnite bilo koju tipku\t')    
 
 
 def display_historical_transactions():
-    pass
+    clear_terminal()
+    print('*' * 65)
+    print('PyBank Algebra\n\n'.center(65))
+    print('Prikaz svih transakcija\n'.center(65))
+
+    for tx in transactions: 
+        print(f'Dana\t{datetime.datetime.now().strftime("%Y-%m-%d")} stanje na racunu promijenilo se za {transactions[tx + 1]}, ukupno stanje racuna \t{account_balance}{currency}\n\n')
+
+    print('_' * 65)
+    input('Za povratak u glavni izbornik pritisnite bilo koju tipku\t')
+
+def create_deposit_transaction():
+    global account_balance
+    
+    print(f'Vase trenutno stanje racuna je {account_balance}{currency}')
+    uplata = float(input('Unesite iznos koji zelite staviti na racun:\t\t'))
+    
+    if uplata != '':
+        uplata = float(uplata)
+        
+        transactions = []
+        account_balance += uplata
+        
+        transactions.append(datetime.datetime.date)
+        transactions.append(datetime.datetime.time)
+        transactions.append(uplata)
+        transactions.append(account_balance)
+        transactions.append(account_number)
+        transactions.append('Polog kod otvaranja racuna')
+        transactions.append(company_manager)
+        
+        transactions[transaction_id + 1] = transactions
+        
+    else:
+        print(f'Uplata u iznosu od {uplata}{currency} nije izvrsena')
+        print('Molimo pokusajte ponovno')
+        
+    create_deposit_transaction()
+
+def create_withdrawal_transaction():
+    global account_balance
+    
+    print(f'Vase trenutno stanje racuna je {account_balance}{currency}')
+    isplata = float(input('Unesite iznos koji zelite podici s racuna:\t\t'))
+    
+    if isplata != '':
+        isplata = float(isplata)
+        
+        transactions = []
+        account_balance -= isplata
+        
+        transactions.append(datetime.datetime.date)
+        transactions.append(datetime.datetime.time)
+        transactions.append(isplata)
+        transactions.append(account_balance)
+        transactions.append(account_number)
+        transactions.append('Polog kod otvaranja racuna')
+        transactions.append(company_manager)
+        
+        transactions[transaction_id + 1] = transactions
+        
+    else:
+        print(f'Isplata u iznosu od {isplata}{currency} nije izvrsena')
+        print('Molimo pokusajte ponovno')
+        
+    create_withdrawal_transaction()
 
 choice = main_menu()
 while choice != 0:
     if choice == 1 and company_name == '':
         open_account()
-    if choice == 1 and company_name != '':
-        # update_account()
+    elif choice == 1 and company_name != '':
+        update_account()
         pass
-    if choice == 2:
+    elif choice == 2:
         display_account_balance()
-    if choice == 3:
+    elif choice == 3:
         display_historical_transactions()
-    if choice == 4:
-        # create_deposit_transaction()
+    elif choice == 4:
+        create_deposit_transaction()
         pass
-    if choice == 5:
-        # create_withdrawal_transaction()
+    elif choice == 5:
+        create_withdrawal_transaction()
         pass
 
     choice = main_menu()
