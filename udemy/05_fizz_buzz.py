ed742542e0c93669d47c81835d51fd06e5e3a88b
Ivan <pinjamar@gmail.com>
2023-06-28 20:38:22 +0200

    day 5 python solved

---
+++ udemy/05_fizz_buzz.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number between 1 and 100: "))
+
+for number in range(1, num):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
