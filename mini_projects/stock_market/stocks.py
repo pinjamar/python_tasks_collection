56d9db1d1c28f8f48d02975595e216ae54462673
Ivan <pinjamar@gmail.com>
2023-11-28 22:17:45 +0100

    added inputpanel

--- mini_projects/stock_market/stocks.py
+++ mini_projects/stock_market/stocks.py
@@ -15,6 +15,12 @@ class App(ctk.CTk):
         # self.iconbitmap('empty.ico')
         self.title_bar_color()
 
+        self.input_string = ctk.StringVar(value='AAPL')
+        self.time_string = ctk.StringVar(value=TIME_OPTIONS[0])
+
+        # widgets
+        InputPanel(self, self.input_string, self.time_string)
+
         self.mainloop()
 
     def title_bar_color(self):
@@ -24,6 +30,16 @@ class App(ctk.CTk):
                 HWND, 35, byref(c_int(TITLE_HEX_COLOR)), sizeof(c_int))
         except:
             pass
+
+
+class InputPanel(ctk.CTkFrame):
+    def __init__(self, parent, input_string, time_string):
+        super().__init__(master=parent, fg_color=INPUT_BG_COLOR, corner_radius=0)
+        self.pack(fill='both', side='bottom')
+
+        # widgets
+        ctk.CTkEntry(self, textvariable=input_string, fg_color=BG_COLOR, border_color=TEXT_COLOR, border_width=1).pack(
+            side='left', padx=10, pady=10)
 
 
 App()
