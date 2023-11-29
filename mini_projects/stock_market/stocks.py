dc65ba3d8f618976cc8d3879f55939f7bcbe733b
Ivan <pinjamar@gmail.com>
2023-11-29 22:53:36 +0100

    added buttons to panel

--- mini_projects/stock_market/stocks.py
+++ mini_projects/stock_market/stocks.py
@@ -40,6 +40,34 @@ class InputPanel(ctk.CTkFrame):
         # widgets
         ctk.CTkEntry(self, textvariable=input_string, fg_color=BG_COLOR, border_color=TEXT_COLOR, border_width=1).pack(
             side='left', padx=10, pady=10)
+        self.buttons = [TextButton(self, text, time_string)
+                        for text in TIME_OPTIONS]
+        time_string.trace('w', self.unselect_all_buttons)
+
+    def unselect_all_buttons(self, *args):
+        for button in self.buttons:
+            button.unselect()
+
+
+class TextButton(ctk.CTkLabel):
+    def __init__(self, parent, text, time_string):
+        super().__init__(master=parent, text=text, text_color=TEXT_COLOR)
+        self.pack(side='right', padx=10, pady=10)
+
+        self.bind('<Button>', self.select_handler)
+
+        self.time_string = time_string
+        self.text = text
+
+        if time_string.get() == text:
+            self.select_handler()
+
+    def select_handler(self, event=None):
+        self.time_string.set(self.text)
+        self.configure(text_color=HIGHLIGHT_COLOR)
+
+    def unselect(self):
+        self.configure(text_color=TEXT_COLOR)
 
 
 App()
