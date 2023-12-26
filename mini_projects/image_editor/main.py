9f70434f707f4aa82361acd17d4ed3aae5bb890f
Ivan <pinjamar@gmail.com>
2023-12-26 16:30:04 +0100

    image editor setup

---
+++ mini_projects/image_editor/main.py
@@ -0,0 +1,28 @@
+import customtkinter as ctk
+from image_widgets import *
+
+class App(ctk.CTk):
+	def __init__(self):
+
+		# setup 
+		super().__init__()
+		ctk.set_appearance_mode('dark')
+		self.geometry('1000x600')
+		self.title('Photo Editor')
+		self.minsize(800,500)
+
+		# layout 
+		self.rowconfigure(0, weight = 1)
+		self.columnconfigure(0, weight = 2)
+		self.columnconfigure(1, weight = 6)
+
+		# widgets 
+		ImageImport(self, self.import_image)
+
+		# run
+		self.mainloop()
+
+	def import_image(self, path):
+		print(path)
+
+App()
