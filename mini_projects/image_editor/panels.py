5ac3b7f7d119c26b8032f386061bbdbea2013c56
Ivan <pinjamar@gmail.com>
2024-01-05 19:31:25 +0100

    data manipulation added

--- mini_projects/image_editor/panels.py
+++ mini_projects/image_editor/panels.py
@@ -7,7 +7,7 @@ class Panel(ctk.CTkFrame):
 		self.pack(fill = 'x', pady = 4, ipady = 8)
 
 class SliderPanel(Panel):
-	def __init__(self, parent, text):
+	def __init__(self, parent, text, data_var, min_value, max_value):
 		super().__init__(parent = parent)
 
 		# layout 
@@ -15,5 +15,15 @@ class SliderPanel(Panel):
 		self.columnconfigure((0,1), weight = 1)
 
 		ctk.CTkLabel(self, text = text).grid(column = 0, row = 0, sticky = 'W', padx = 5)
-		ctk.CTkLabel(self, text = '0.0').grid(column = 1, row = 0, sticky = 'E', padx = 5)
-		ctk.CTkSlider(self, fg_color = SLIDER_BG).grid(row = 1, column = 0, columnspan = 2, sticky = 'ew', padx = 5, pady = 5)
+		self.num_label = ctk.CTkLabel(self, text = data_var.get())
+		self.num_label.grid(column = 1, row = 0, sticky = 'E', padx = 5)
+		
+		ctk.CTkSlider(self, 
+			fg_color = SLIDER_BG, 
+			variable = data_var,
+			from_ = min_value,
+			to = max_value,
+			command = self.update_text).grid(row = 1, column = 0, columnspan = 2, sticky = 'ew', padx = 5, pady = 5)
+
+	def update_text(self, value):
+		self.num_label.configure(text = f'{round(value, 2)}')
